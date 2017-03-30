138f44ec807d5f00d95ee897ee428096f7913942
Brian Edwards <git@edwardstx.us>
2017-03-30 22:21:18 +0300

    Added service key func test

--- test/us/edwardstx/auth/authentication_func_test.clj
+++ test/us/edwardstx/auth/authentication_func_test.clj
@@ -4,6 +4,8 @@
             [us.edwardstx.test.db :refer [new-test-database]]
             [com.stuartsierra.component :as component]
             [one-time.core :as ot]
+            [buddy.sign.jwt :as jwt]
+            [clj-time.core :as time]
             [manifold.deferred :as d]))
 
 (def db-conf
@@ -17,12 +19,18 @@
    (component/system-map
     :db (new-test-database conf))))
 
+(defn generate-ksr [service]
+   {:sub service
+    :iss service
+    :jti (str (java.util.UUID/randomUUID))
+    :exp (time/plus (time/now) (time/minutes 1))})
+
 
 (defonce system (atom nil))
 
 (against-background [(before :contents (reset! system (init-db db-conf)))
                      (after :contents (component/stop @system))]
-                    (facts "authenticate"
+                    (facts "authenticate" :func
                            (let [db (:db @system)
                                  conf (:conf db)
                                  {:keys [email password secret]} (first (:users conf))
@@ -34,4 +42,28 @@
                              (fact "Invalid password"
                                    @(auth/authenticate db email (str password "C") code) => false)
                              (fact "Invalid auth"
-                                   @(auth/authenticate db email password (+ 1 code)) => false))))
+                                   @(auth/authenticate db email password (+ 1 code)) => false)))
+                    (facts "authenticate-update-password!" :func
+                           (let [db (:db @system)
+                                 conf (:conf db)
+                                 {:keys [email password secret]} (last (:users conf))
+                                 npass (str "new_" password "_updates")
+                                 code (ot/get-totp-token secret)]
+                             (fact "authenticate-update-password!"
+                                   (do
+                                     @(auth/authenticate-update-password! db email password npass code)
+                                     @(auth/authenticate db email npass code)) => true)))
+                    (facts "validate-token" :func
+                           (let [db (:db @system)
+                                 conf (:conf db)
+                                 service-name "hvac_daemon"
+                                 service (-> conf :services (get "hvac_daemon"))
+                                 ksr (generate-ksr service-name)
+                                 token (jwt/sign ksr (:private-key service)  {:alg :es256})
+                                 claims (dissoc  (assoc ksr :key (:public-key-base64 service)) :exp )]
+                             (dissoc @(auth/validate-token db service-name token) :exp) => claims))
+
+
+
+
+                    )
